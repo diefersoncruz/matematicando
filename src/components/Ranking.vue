8f7ae0ca83e2162c361ca57f13e80c4ae7f418d8
Dieferson Cruz <dieferson.cruz@outlook.com>
2025-03-07 23:34:20 -0300

    Adicionado dependencia do Element Plus. Alterado component de rankin para usar o element plus.

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -1,70 +1,27 @@
 <template>
-  <div class="ranking-container">
-    <h3 class="ranking-title">Ranking da Sala {{ sala }}</h3>
-    <div class="ranking-table-wrapper">
-      <table class="ranking-table">
-        <thead class="ranking-table-header">
-          <tr>
-            <th>Posição</th>
-            <th>Nome</th>
-            <th>Acertos</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(aluno, index) in alunosOrdenados" :key="aluno.id">
-            <td>{{ index + 1 }}</td>
-            <td>{{ aluno.nome }}</td>
-            <td>{{ aluno.acertos }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+  <h1 class="title">Sala atual: 1b</h1>
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+    height="100%"
+    :row-class-name="tableRowClassName"
+  >
+    <el-table-column prop="position" label="Pos." width="50" />
+    <el-table-column prop="name" label="Nome" width="170" />
+    <el-table-column prop="hits" label="Acertos" width="80" />
+  </el-table>
 </template>
 
-<script>
-import "@/assets/css/Ranking.css";
-export default {
-  props: {
-    sala: {
-      type: String,
-      required: true,
-    },
-    alunos: {
-      type: Array,
-      required: true,
-    },
-  },
-  computed: {
-    alunosOrdenados() {
-      // Verifica se alunos é definido antes de ordenar
-      if (this.alunos) {
-        return this.alunos.sort((a, b) => b.acertos - a.acertos);
-      } else {
-        return [
-          { id: 3, nome: "Pedro", acertos: 35 },
-          { id: 2, nome: "Maria", acertos: 20 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-          { id: 1, nome: "João", acertos: 15 },
-        ]; // Retorna um array vazio se alunos for undefined
-      }
-    },
-  },
-};
+<script setup>
+import { ref } from "vue";
+
+const tableData = ref([{ position: 1, name: "João", hits: 10 }]);
 </script>
+
+<style scoped>
+.title {
+  text-align: center;
+  border: 1rem solid;
+  border-color: transparent;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./assets/css/style.css"; // Importação do CSS global
+import "./assets/css/style.css";
 
 const app = createApp(App);
 
