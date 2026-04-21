9520aeff9fe1264122a498daf6edee8e21074ce3
Dieferson Cruz <dieferson.cruz@outlook.com>
2026-04-20 22:56:16 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-// https://vite.dev/config/
+// https://vite.dev/config/ - teste
 export default defineConfig({
   base: "/",
   plugins: [
